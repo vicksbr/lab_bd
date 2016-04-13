14f65f4a76a7253a22bd943b6af8ce53dde90c2c
vicksbr <pedropuzzi@gmail.com>
2016-04-13 19:03:28 -0300

    Update trab4_git.sql

--- trab4_git.sql
+++ trab4_git.sql
@@ -16,21 +16,25 @@ END;
 
 set serveroutput on
 
-CREATE OR REPLACE PROCEDURE calcula_imposto (valor in number, estado_id in number) AS
+create or replace PROCEDURE calcula_imposto (p_valor in number, p_estado_id in number) AS
 
   u_estado_id NUMBER(38,2);
   u_imposto_nome VARCHAR2(50);
   u_taxa NUMBER(38,2);
+  u_valor_calculado NUMBER := 0;
   
-  CURSOR vcursor IS SELECT ESTADO_ID, NOME, TAXA FROM IMPOSTO WHERE ESTADO_ID=estado_id;
+  CURSOR vcursor IS SELECT ESTADO_ID, NOME, TAXA FROM IMPOSTO WHERE ESTADO_ID=p_estado_id AND TIPO BETWEEN 1 AND 3;
   
 BEGIN
     OPEN vcursor;
     LOOP
       FETCH vcursor INTO u_estado_id, u_imposto_nome, u_taxa;
       EXIT WHEN vcursor%notfound;
-      dbms_output.put_line('Nome: ' || u_imposto_nome  || ' Valor: '|| u_taxa );
+      u_valor_calculado := u_valor_calculado + u_taxa*p_valor;
+      dbms_output.put_line('Nome: ' || u_imposto_nome  || ' Valor: '|| p_valor*u_taxa ); 
     END LOOP;
     CLOSE vcursor;
+     dbms_output.put_line('Valor sem imposto: ' || p_valor ); 
+     dbms_output.put_line('Valor com imposto: ' || (u_valor_calculado + p_valor)); 
 END;
 
