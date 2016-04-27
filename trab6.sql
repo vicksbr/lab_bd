315432f2d319989d15471c05aee0346de175c608
vicksbr <pedropuzzi@gmail.com>
2016-04-27 16:16:55 -0300

    Update trab6.sql

--- trab6.sql
+++ trab6.sql
@@ -47,4 +47,21 @@ DROP TABLE temp;
 
 5)
 
-        
+CREATE OR REPLACE FUNCTION identificaTipo RETURN VARCHAR2 IS
+    hashhh VARCHAR2(128 BYTE);
+    pessoa_id number(38,2);
+    email VARCHAR2(50 BYTE);
+    login VARCHAR2(250 BYTE);
+    retorno number(38,2);
+BEGIN
+    SELECT PESSOA.PASSWORD_HASH, PESSOA.PESSOA_ID, PESSOA.EMAIL INTO hashhh,pessoa_id,email FROM PESSOA WHERE PESSOA_ID = 79;
+    dbms_output.put_line(hashhh);    
+    
+    BEGIN
+      SELECT LOGIN_ID INTO login FROM EMPREGADO WHERE PESSOA_ID=79;
+      EXCEPTION 
+        WHEN NO_DATA_FOUND THEN    
+          dbms_output.put_line('nao eh funcionario');            
+          RETURN 'cliente'
+    END;
+END;        
